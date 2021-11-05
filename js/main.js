e7dbbf9ff9a1aa35d064fbb9a08355785b985ca6
Gowtham Ravindrathas <39655386+gowth6m@users.noreply.github.com>
2021-11-05 02:28:53 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $(window).scroll(function() {
 //   ===================
 
 // Set the date we're counting down to
-var countDownDate = new Date("November 6, 2021 15:00:00").getTime();
+var countDownDate = new Date("November 5, 2021 15:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
